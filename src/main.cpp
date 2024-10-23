5b2a20305fdd97b1b49761e0f89e234a0880a703
Rakshit Raj <154298128+memeliciouss@users.noreply.github.com>
2024-10-23 14:03:40 +0530

    motor driver tweaks

--- src/main.cpp
+++ src/main.cpp
@@ -19,7 +19,7 @@ float ki=1;
 float kd=1;
 
 void readEncB();
-void setMotor(int dir, int pwmVal, int pwm, int m1, int m2);
+void setMotor(int dir, int pwmVal, int pwm_pin, int dir_pin);
 
 void setup() {
   Serial.begin(9600);
