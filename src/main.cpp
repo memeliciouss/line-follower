69981e44cfd0d3e80cc373ac2be6b2a9d4e9734c
memeliciouss <rakshit3raj@gmail.com>
2025-03-26 15:13:42 +0530

    updated

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,42 @@
 #include <Arduino.h>
+const int led_pin = PB5;
 
-const int LED_PIN=13;
+//Counter and compare values
+const uint16_t t1_load = 0;
+const uint16_t t1_comp = 6250;
 
 void setup() {
-  Serial.begin(9600);
-
-  TCCR1A = 0;
-  TCCR1B = 0;
-
-  // Set CTC mode
-  TCCR1B &= ~(1 << WGM13);  // Clear WGM13
-  TCCR1B |= (1 << WGM12);   // Set WGM12 for CTC mode
-  
-  // Set prescaler to 256
-  TCCR1B |= (1 << CS12);
-  TCCR1B &= ~(1 << CS11);
-  TCCR1B &= ~(1 << CS10);
-  
-  // Set OCR1A to 32000 (32000/256) for interrupts at every 2 mili seconds
-  OCR1A = 125;
-  
-  // Enable Timer1 compare interrupt
-  TIMSK1 |= (1 << OCIE1A);
-  
-  sei();
+	//Set LED pin to be output
+	DDRB |= (1<<led_pin);
+	
+	//Reset Timer Control Reg A
+	TCCR1A=0;
+  TCCR1B &= ~(1 << WGM13);
+  TCCR1B |= (1 << WGM12);
+	
+	//Set to prescaler of 256
+	TCCR1B |= (1 << CS12);
+	TCCR1B &= ~(1 << CS11);
+	TCCR1B &= (~1 << CS10);
+	
+	//Reset Timer1 and set compare values
+	TCNT1 = t1_load;
+	OCR1A = t1_comp;
+	
+	//Enable Timer1 compare interrupt
+	TIMSK1 = (1 << OCIE1A);
+	
+	//Enable global interrupts
+	sei();
 }
 
 void loop() {
-  
+	delay(500);
 }
 
 ISR(TIMER1_COMPA_vect) {
-  digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+	
+	//toggle LED by only flipping that bit
+	// ^= is XOR operator
+	PORTB ^= (1 << led_pin);
 }
